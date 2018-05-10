bddaa31c01dfe039b8c2bdb76e9f960835db0182
ChicksMagee <tesamra@gmail.com>
2018-05-09 22:32:40 -0700

    some changes

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -11,6 +11,10 @@
 #include <player.h>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+#include <queue>
+#include <math.h>
+#include <ctime>
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
@@ -27,13 +31,25 @@
 /* GLUT callback Handlers */
 
 using namespace std;
-
+const int n = 20; // horizontal size of the map
+const int m = 20; // vertical size size of the map
+static int map[n][m];
+static int closed_nodes_map[n][m]; // map of closed (tried-out) nodes
+static int open_nodes_map[n][m]; // map of open (not-yet-tried) nodes
+static int dir_map[n][m]; // map of directions
+const int dir = 4; // number of possible directions to go at any position
+				   // if dir==4
+				   static int dx[dir]={1, 0, -1, 0};
+				   static int dy[dir]={0, 1, 0, -1};
+				   // if dir==8
+//static int dx[dir] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+//static int dy[dir] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 Maze *M = new Maze(20);                         // Set Maze grid size
 //Maze *M1 = new Maze(20);                         // Set Background grid size
 Player *P = new Player();                       // create player
 
-wall W[10000];
-int wallArray[50][50];                                    // wall with number of bricks
+wall W[1000];
+//int wallArray[50][50];                                    // wall with number of bricks
 Enemies E[100];                                  // create number of enemies
 Timer *T0 = new Timer();
  string line, obj;
@@ -44,6 +60,216 @@ Timer *T0 = new Timer();
 float wWidth, wHeight;                          // display window width and Height
 float xPos,yPos;                                // Viewpoar mapping
 
+void rotateMatrix(int mat[n][m])
+{
+    // Consider all squares one by one
+    for (int x = 0; x < n / 2; x++)
+    {
+        // Consider elements in group of 4 in
+        // current square
+        for (int y = x; y < n-x-1; y++)
+        {
+            // store current cell in temp variable
+            int temp = mat[x][y];
+
+            // move values from right to top
+            mat[x][y] = mat[y][n-1-x];
+
+            // move values from bottom to right
+            mat[y][n-1-x] = mat[n-1-x][n-1-y];
+
+            // move values from left to bottom
+            mat[n-1-x][n-1-y] = mat[n-1-y][x];
+
+            // assign temp to left
+            mat[n-1-y][x] = temp;
+        }
+    }
+}
+
+class node
+{
+	// current position
+	int xPos;
+	int yPos;
+	// total distance already travelled to reach the node
+	int level;
+	// priority=level+remaining distance estimate
+	int priority;  // smaller: higher priority
+
+public:
+	node(int xp, int yp, int d, int p)
+	{
+		xPos = xp; yPos = yp; level = d; priority = p;
+	}
+
+	int getxPos() const { return xPos; }
+	int getyPos() const { return yPos; }
+	int getLevel() const { return level; }
+	int getPriority() const { return priority; }
+
+	void updatePriority(const int & xDest, const int & yDest)
+	{
+		priority = level + estimate(xDest, yDest) * 10; //A*
+	}
+
+	// give better priority to going strait instead of diagonally
+	void nextLevel(const int & i) // i: direction
+	{
+		level += (dir == 8 ? (i % 2 == 0 ? 10 : 14) : 10);
+	}
+
+	// Estimation function for the remaining distance to the goal.
+	const int & estimate(const int & xDest, const int & yDest) const
+	{
+		static int xd, yd, d;
+		xd = xDest - xPos;
+		yd = yDest - yPos;
+
+		// Euclidian Distance
+		//d = static_cast<int>(sqrt(xd*xd + yd*yd));
+
+		// Manhattan distance
+		d=abs(xd)+abs(yd);
+
+		// Chebyshev distance
+		//d=max(abs(xd), abs(yd));
+
+		return(d);
+	}
+};
+
+// Determine priority (in the priority queue)
+bool operator<(const node & a, const node & b)
+{
+	return a.getPriority() > b.getPriority();
+}
+
+// A-star algorithm.
+// The route returned is a string of direction digits.
+string pathFind(const int & xStart, const int & yStart,
+	const int & xFinish, const int & yFinish)
+{
+	static priority_queue<node> pq[2]; // list of open (not-yet-tried) nodes
+	static int pqi; // pq index
+	static node* n0;
+	static node* m0;
+	static int i, j, x, y, xdx, ydy;
+	static char c;
+	pqi = 0;
+
+	// reset the node maps
+	for (y = 0; y<m; y++)
+	{
+		for (x = 0; x<n; x++)
+		{
+			closed_nodes_map[x][y] = 0;
+			open_nodes_map[x][y] = 0;
+		}
+	}
+
+	// create the start node and push into list of open nodes
+	n0 = new node(xStart, yStart, 0, 0);
+	n0->updatePriority(xFinish, yFinish);
+	pq[pqi].push(*n0);
+	open_nodes_map[x][y] = n0->getPriority(); // mark it on the open nodes map
+
+											  // A* search
+	while (!pq[pqi].empty())
+	{
+		// get the current node w/ the highest priority
+		// from the list of open nodes
+		n0 = new node(pq[pqi].top().getxPos(), pq[pqi].top().getyPos(),
+			pq[pqi].top().getLevel(), pq[pqi].top().getPriority());
+
+		x = n0->getxPos(); y = n0->getyPos();
+
+		pq[pqi].pop(); // remove the node from the open list
+		open_nodes_map[x][y] = 0;
+		// mark it on the closed nodes map
+		closed_nodes_map[x][y] = 1;
+
+		// quit searching when the goal state is reached
+		//if((*n0).estimate(xFinish, yFinish) == 0)
+		if (x == xFinish && y == yFinish)
+		{
+			// generate the path from finish to start
+			// by following the directions
+			string path = "";
+			while (!(x == xStart && y == yStart))
+			{
+				j = dir_map[x][y];
+				c = '0' + (j + dir / 2) % dir;
+				path = c + path;
+				x += dx[j];
+				y += dy[j];
+			}
+
+			// garbage collection
+			delete n0;
+			// empty the leftover nodes
+			while (!pq[pqi].empty()) pq[pqi].pop();
+			return path;
+		}
+
+		// generate moves (child nodes) in all possible directions
+		for (i = 0; i<dir; i++)
+		{
+			xdx = x + dx[i]; ydy = y + dy[i];
+
+			if (!(xdx<0 || xdx>n - 1 || ydy<0 || ydy>m - 1 || map[xdx][ydy] == 1
+				|| closed_nodes_map[xdx][ydy] == 1))
+			{
+				// generate a child node
+				m0 = new node(xdx, ydy, n0->getLevel(),
+					n0->getPriority());
+				m0->nextLevel(i);
+				m0->updatePriority(xFinish, yFinish);
+
+				// if it is not in the open list then add into that
+				if (open_nodes_map[xdx][ydy] == 0)
+				{
+					open_nodes_map[xdx][ydy] = m0->getPriority();
+					pq[pqi].push(*m0);
+					// mark its parent node direction
+					dir_map[xdx][ydy] = (i + dir / 2) % dir;
+				}
+				else if (open_nodes_map[xdx][ydy]>m0->getPriority())
+				{
+					// update the priority info
+					open_nodes_map[xdx][ydy] = m0->getPriority();
+					// update the parent direction info
+					dir_map[xdx][ydy] = (i + dir / 2) % dir;
+
+					// replace the node
+					// by emptying one pq to the other one
+					// except the node to be replaced will be ignored
+					// and the new node will be pushed in instead
+					while (!(pq[pqi].top().getxPos() == xdx &&
+						pq[pqi].top().getyPos() == ydy))
+					{
+						pq[1 - pqi].push(pq[pqi].top());
+						pq[pqi].pop();
+					}
+					pq[pqi].pop(); // remove the wanted node
+
+								   // empty the larger size pq to the smaller one
+					if (pq[pqi].size()>pq[1 - pqi].size()) pqi = 1 - pqi;
+					while (!pq[pqi].empty())
+					{
+						pq[1 - pqi].push(pq[pqi].top());
+						pq[pqi].pop();
+					}
+					pqi = 1 - pqi;
+					pq[pqi].push(*m0); // add the better node instead
+				}
+				else delete m0; // garbage collection
+			}
+		}
+		delete n0; // garbage collection
+	}
+	return ""; // no route found
+}
 
 void display(void);                             // Main Display : this runs in a loop
 
@@ -81,6 +307,12 @@ void init()
     fstream myfile ("maze.txt");
 if (myfile.is_open()){
     cout << "Able to open file" << endl;
+     // create empty map
+        for (int y = 0; y<m; y++)
+	{
+		for (int x = 0; x<n; x++) map[x][y] = 0;
+	}
+
     while ( getline (myfile,line) ){
         stringstream ss(line);
         ss >> obj >> x >> y;
@@ -95,8 +327,9 @@ if (myfile.is_open()){
        else if(obj == "wall"){
             W[numWall].wallInit(M->getGridSize(),"images/wall.png");// Load walls
             W[numWall].placeWall(x,y);
-            wallArray[x][y] = 1;
-            numWall++;                              // place each brick
+            //wallArray[x][y] = 1;
+            numWall++;
+            map[x][y] = 1;// place each brick
             }
         else if(obj =="arrow"){
             M->loadSetOfArrowsImage("images/arrow.png");      // load set of arrows image
@@ -112,6 +345,13 @@ if (myfile.is_open()){
             M->placeChest(x,y);
             }
         }
+       //S rotateMatrix(map);
+        for (int i = 0; i < n; ++i){
+        for (int j = 0; j < m; ++j){
+            std::cout << map[i][j] << ' ';
+        }
+        std::cout << std::endl;
+	}
     myfile.close();
     }
 }
@@ -125,7 +365,7 @@ void display(void)
         glPopMatrix();
 
 
-        for(int i=0; i<1000;i++)
+        for(int i=0; i<500;i++)
         {
            W[i].drawWall();
         }
@@ -138,7 +378,7 @@ void display(void)
             P->drawplayer();
         glPopMatrix();
 
-        for(int i=0; i<100;i++)
+        for(int i=0; i<10;i++)
         {
         E[i].drawEnemy();
         }
@@ -237,9 +477,9 @@ void Specialkeys(int key, int x, int y){
     switch(key)
     {
     case GLUT_KEY_UP:
-        if(P->livePlayer){
+        if(P->livePlayer && M->liveChest){
          cout<< P->getPlayerLoc().x<< "    "<<P->getPlayerLoc().y<<endl;
-         if(wallArray[P->getPlayerLoc().x][P->getPlayerLoc().y + 1] == 1 ){} // do nothing, hit wall
+         if(map[P->getPlayerLoc().x][P->getPlayerLoc().y + 1] == 1 ){} // do nothing, hit wall
          else{P->movePlayer("up",P->frames);} // move up
          if((P->getPlayerLoc().x == M->GetChestLoc().x) && (P->getPlayerLoc().y == M->GetChestLoc().y)){
             M->liveChest = false;
@@ -252,14 +492,26 @@ void Specialkeys(int key, int x, int y){
             for(int i = 0; i < numEnemy; i++){
          if((P->getPlayerLoc().x == E[i].getEnemyLoc().x) && (P->getPlayerLoc().y == E[i].getEnemyLoc().y)){
             P->livePlayer = false;}
+        else{
+            string route = pathFind( E[i].getEnemyLoc().x, E[i].getEnemyLoc().y , P->getPlayerLoc().x, P->getPlayerLoc().y);
+           // cout << "Map Size (X,Y): " << n << "," << m << endl;
+            cout << "Start: " << E[i].getEnemyLoc().x << "," << E[i].getEnemyLoc().y << endl;
+            cout << "Finish: " << P->getPlayerLoc().x << "," << P->getPlayerLoc().y << endl;
+           // cout << i << endl;
+             cout << route[0] << endl;
+           if(route[0] == '0'){E[0].moveEnemy("right");}
+            else if(route[0] == '1'){E[0].moveEnemy("down");}
+             else if(route[0] == '2'){E[0].moveEnemy("left");}
+            else{E[0].moveEnemy("up");}
+        }
             }
         }
     break;
 
     case GLUT_KEY_DOWN:
-        if(P->livePlayer){
+        if(P->livePlayer && M->liveChest){
          cout<< P->getPlayerLoc().x<< "    "<<P->getPlayerLoc().y<<endl;
-          if(wallArray[P->getPlayerLoc().x][P->getPlayerLoc().y - 1] == 1 ){} // do nothing hit wall
+          if(map[P->getPlayerLoc().x][P->getPlayerLoc().y - 1] == 1 ){} // do nothing hit wall
           else{P->movePlayer("down",P->frames);} // move down
           if((P->getPlayerLoc().x == M->GetChestLoc().x) && (P->getPlayerLoc().y == M->GetChestLoc().y)){
             M->liveChest = false;
@@ -272,14 +524,26 @@ void Specialkeys(int key, int x, int y){
          for(int i = 0; i < numEnemy; i++){
          if((P->getPlayerLoc().x == E[i].getEnemyLoc().x) && (P->getPlayerLoc().y == E[i].getEnemyLoc().y)){
             P->livePlayer = false;}
+              else{
+            string route = pathFind( E[i].getEnemyLoc().x, E[i].getEnemyLoc().y , P->getPlayerLoc().x, P->getPlayerLoc().y);
+            //cout << "Map Size (X,Y): " << n << "," << m << endl;
+            cout << "Start: " << E[i].getEnemyLoc().x << "," << E[i].getEnemyLoc().y << endl;
+            cout << "Finish: " << P->getPlayerLoc().x << "," << P->getPlayerLoc().y << endl;
+           // cout << i << endl;
+            cout << route[0] << endl;
+          if(route[0] == '0'){E[0].moveEnemy("right");}
+            else if(route[0] == '1'){E[0].moveEnemy("down");}
+             else if(route[0] == '2'){E[0].moveEnemy("left");}
+            else{E[0].moveEnemy("up");}
+        }
             }
         }
     break;
 
     case GLUT_KEY_LEFT:
-        if(P->livePlayer){
+        if(P->livePlayer && M->liveChest){
          cout<< P->getPlayerLoc().x<< "    "<<P->getPlayerLoc().y<<endl;
-          if(wallArray[P->getPlayerLoc().x - 1][P->getPlayerLoc().y] == 1 ){} // do nothing hit wall
+          if(map[P->getPlayerLoc().x - 1][P->getPlayerLoc().y] == 1 ){} // do nothing hit wall
           else{P->movePlayer("left",P->frames);} // move left
           if((P->getPlayerLoc().x == M->GetChestLoc().x) && (P->getPlayerLoc().y == M->GetChestLoc().y)){
             M->liveChest = false;
@@ -292,14 +556,26 @@ void Specialkeys(int key, int x, int y){
        for(int i = 0; i < numEnemy; i++){
          if((P->getPlayerLoc().x == E[i].getEnemyLoc().x) && (P->getPlayerLoc().y == E[i].getEnemyLoc().y)){
             P->livePlayer = false;}
+              else{
+            string route = pathFind( E[i].getEnemyLoc().x, E[i].getEnemyLoc().y , P->getPlayerLoc().x, P->getPlayerLoc().y);
+           // cout << "Map Size (X,Y): " << n << "," << m << endl;
+            cout << "Start: " << E[i].getEnemyLoc().x << "," << E[i].getEnemyLoc().y << endl;
+            cout << "Finish: " << P->getPlayerLoc().x << "," << P->getPlayerLoc().y << endl;
+           // cout << i << endl;
+             cout << route[0] << endl;
+            if(route[0] == '0'){E[0].moveEnemy("right");}
+            else if(route[0] == '1'){E[0].moveEnemy("down");}
+             else if(route[0] == '2'){E[0].moveEnemy("left");}
+            else{E[0].moveEnemy("up");}
+        }
             }
         }
     break;
 
     case GLUT_KEY_RIGHT:
-        if(P->livePlayer){
+        if(P->livePlayer && M->liveChest){
          cout<< P->getPlayerLoc().x<< "    "<<P->getPlayerLoc().y<<endl;
-          if(wallArray[P->getPlayerLoc().x + 1][P->getPlayerLoc().y] == 1 ){} // do nothing hit wall
+          if(map[P->getPlayerLoc().x + 1][P->getPlayerLoc().y] == 1 ){} // do nothing hit wall
           else{P->movePlayer("right",P->frames);} // move right
           if((P->getPlayerLoc().x == M->GetChestLoc().x) && (P->getPlayerLoc().y == M->GetChestLoc().y)){
             M->liveChest = false;
@@ -312,6 +588,18 @@ void Specialkeys(int key, int x, int y){
        for(int i = 0; i < numEnemy; i++){
          if((P->getPlayerLoc().x == E[i].getEnemyLoc().x) && (P->getPlayerLoc().y == E[i].getEnemyLoc().y)){
             P->livePlayer = false;}
+              else{
+            string route = pathFind( E[i].getEnemyLoc().x, E[i].getEnemyLoc().y , P->getPlayerLoc().x, P->getPlayerLoc().y);
+           // cout << "Map Size (X,Y): " << n << "," << m << endl;
+            cout << "Start: " << E[i].getEnemyLoc().x << "," << E[i].getEnemyLoc().y << endl;
+            cout << "Finish: " << P->getPlayerLoc().x << "," << P->getPlayerLoc().y << endl;
+           // cout << i << endl;
+            cout << route[0] << endl;
+           if(route[0] == '0'){E[0].moveEnemy("right");}
+            else if(route[0] == '1'){E[0].moveEnemy("down");}
+             else if(route[0] == '2'){E[0].moveEnemy("left");}
+            else{E[0].moveEnemy("up");}
+        }
             }
         }
     break;
